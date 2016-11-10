6c39294bdf273bf0807be2990bad5cf07d68b2eb
yeen <yeen666@kookmin.ac.kr>
2016-11-10 18:21:40 +0900

    complete qucik-cording-templete view

---
+++ quick-cording-templete/src/main/java/com/example/quick_cording_templete/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.quick_cording_templete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class
+MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    EditText in_value;
+
+    ArrayList<String> string_arr = new ArrayList<String>();
+    ArrayList<Integer> int_arr = new ArrayList<Integer>();
+
+    try{
+
+    }
+
+
+}
