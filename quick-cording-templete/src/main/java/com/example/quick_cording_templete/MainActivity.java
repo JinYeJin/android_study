1c54e177181c52dbffd2a1f0e65d91fa9117e1c5
yeen <yeen666@kookmin.ac.kr>
2016-11-11 03:14:25 +0900

    add onClickListener for input button.

--- quick-cording-templete/src/main/java/com/example/quick_cording_templete/MainActivity.java
+++ quick-cording-templete/src/main/java/com/example/quick_cording_templete/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quick_cording_templete;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,15 +16,30 @@ MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText in_value;
+
+
+        final EditText edittext=(EditText)findViewById(R.id.edittext);
+        Button btn_value=(Button)findViewById(R.id.btn_input);
+        final TextView text_number=(TextView)findViewById(R.id.text_number);
+        final TextView text_string=(TextView)findViewById(R.id.text_string)
+
+        final ArrayList<String> string_arr = new ArrayList<String>();
+        final ArrayList<Integer> int_arr = new ArrayList<Integer>();
+
+
+        btn_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    text_number.setText(edittext.getText());
+                    string_arr.add(edittext.getText());
+                }catch(Exception E){
+                    text_string.setText(edittext.getText());
+                    int_arr.add(edittext.getText())
+                }
+            }
+        });
     }
-    EditText in_value;
-
-    ArrayList<String> string_arr = new ArrayList<String>();
-    ArrayList<Integer> int_arr = new ArrayList<Integer>();
-
-    try{
-        int_arr
-    }
-
-
 }
