3e0d9519f79efcdbc95b943f22bd225bd9c7b8f2
yeen <yeen666@kookmin.ac.kr>
2016-11-15 20:44:27 +0900

    complete view

--- quick-cording-04/src/main/java/com/example/quick_cording_04/BankAccount.java
+++ quick-cording-04/src/main/java/com/example/quick_cording_04/BankAccount.java
@@ -4,4 +4,24 @@ package com.example.quick_cording_04;
  * Created by Jin on 11/15/2016.
  */
 public class BankAccount {
+    private int balance = 100;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withDraw(int amount) {
+        balance -= amount;
+    }
+
+    public void add(int money) {
+        balance += money;
+        System.out.println("총합 : " + balance + " " + money + " 만큼 입금");
+    }
+
+    public void delete(int money) {
+        balance -= money;
+        System.out.println("총합 : " + balance + " " + money + " 만큼 출금");
+    }
 }
+
