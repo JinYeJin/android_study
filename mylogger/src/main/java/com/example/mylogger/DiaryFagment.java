17ae10d0b4feb7fa469d4de64ef94696711e7752
yeen <yeen666@kookmin.ac.kr>
2016-11-16 15:56:48 +0900

    add diary fragment

---
+++ mylogger/src/main/java/com/example/mylogger/DiaryFagment.java
@@ -0,0 +1,20 @@
+package com.example.mylogger;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jin on 11/16/2016.
+ */
+
+public class DiaryFagment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_diary, container, false);
+    }
+}
